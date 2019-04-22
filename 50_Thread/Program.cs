ef3ff98c35e6be19448a1ac5d00b7f362b2411c9
Rinat <bloomder@mail.ru>
2019-04-23 00:08:08 +0300

    Change all solution .net framework 4.7.2

---
+++ 50_Thread/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace _50_Thread
+{
+    class Program
+    {
+        static void Method()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Method: " + i.ToString());
+                //Thread.Sleep(1000);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread th1 = new Thread(new ThreadStart(Method));
+            th1.IsBackground = true;
+            th1.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Main: " + i.ToString());
+                //Thread.Sleep(1000);
+            }
+            Console.ReadKey();
+        }
+    }
+}
