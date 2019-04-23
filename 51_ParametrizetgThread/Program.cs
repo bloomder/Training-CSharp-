702f50fd97f75e23b5791dd74aa9bafc445f0c08
bloomder <bloomder@mail.ru>
2019-04-23 15:14:17 +0300

    add 51,52 lessons

---
+++ 51_ParametrizetgThread/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace _51_ParametrizetgThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread th1 = new Thread(new ParameterizedThreadStart(Method1));
+            th1.Start("Hello");
+            th1 = new Thread(new ParameterizedThreadStart(Method1));
+            th1.Start("World");
+            Console.ReadKey();
+        }
+        /* ParameterizedThreadStart - только 1 аргумент
+        static void Method1(object s, int a)
+        {
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine(s);
+                Thread.Sleep(1000);
+            }
+        }
+        */
+        static void Method1(object s)
+        {
+            for (;;)
+            {
+                Console.WriteLine(s);
+                //Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ 52_Thread.isBackground/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace _52_Thread.isBackground
+{
+    class Program
+    {
+        // Thread.IsBackground=false; - не фоновый,это значит поток будет жить 
+        // независмо от жизни основного потока(Main). Поумолчанию он false
+        // Thread.IsBackground=true; - фоновый, это значит, как завершится основной поток(Main)
+        // фоновый поток прекратится
+        static void Main(string[] args)
+        {
+            Thread th1 = new Thread(new ThreadStart(Method));
+            th1.Start();
+            th1.IsBackground = true;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Main");
+                Thread.Sleep(500);
+            }
+        }
+        static void Method()
+        {
+            while(true)
+            {
+                Console.WriteLine("                   Method");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
