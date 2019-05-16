2add08aac8f4d9f06e0fa7d10976bec1398ac018
Rinat <bloomder@mail.ru>
2019-05-16 16:29:39 +0300

    add 74 lesson

---
+++ 74_Anonim_Type/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _74_Anonim_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ins = new { Value = 20, Message = "Hello", Count = 2.45 };
+            Console.WriteLine(ins.Value + " " + ins.Message + " " + ins.Count);
+            object obj = ins;
+            Console.ReadKey();
+        }
+    }
+}
